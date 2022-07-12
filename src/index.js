7a2258cdc752426a28604c5ec44125e2de2d8676
Edio Gama <ediogama@hotmail.com>
2022-07-12 11:58:20 -0300

    add method post create account

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 const express = require("express");
-
+const { v4: uuidV4 } = require("uuid")
 const app = express();
 
 app.use(express.json());
+
+const custumers = [];
+
+app.post("/account", (request, response) => {
+    const { name, cpf } = request.body;
+
+    const customerAlreadyExists = custumers.some(
+        custumer => custumer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
+
+    custumers.push({
+        id: uuidV4(),
+        name,
+        cpf,
+        statement: []
+    });
+
+    return response.status(201).send();
+});
 
 app.listen(3333, console.log("Server is live..."))
